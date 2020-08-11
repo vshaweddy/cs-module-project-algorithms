a5c9c5d43e165f656a6d41c6cf43a39ce0a95388
Vici Shaweddy <vshaweddy@gmail.com>
2020-08-11 19:40:56 -0400

    single number with linear approach

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,23 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # create an empty dictionary
+    dict = {}
+    # loop for number in the array store the number of occured
+    for number in arr:
+        if number in dict: 
+            # count how many times it appears
+            dict[number] += 1
+        else:
+            dict[number] = 1
 
-    pass
+    # loop through the dictionary key and check which one has only one value
+    for number in dict:
+        if dict[number] == 1:    
+            return number
+        
+
+    return None
 
 
 if __name__ == '__main__':
