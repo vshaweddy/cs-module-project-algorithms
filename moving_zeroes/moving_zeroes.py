fafb5c2a30cc50220254642a94a9e43f4c1c514b
Vici Shaweddy <vshaweddy@gmail.com>
2020-08-11 21:40:00 -0400

    moving zeroes O(n) time O(1) space

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,9 +3,22 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # set a new index to zero as a start
+    new_index = 0
 
-    pass
+    # start inserting the number that isn't zero to the new list by looping 
+    # the original list
+
+    for i in range(len(arr)):
+        if arr[i] != 0:
+            arr[new_index] = arr[i]
+            new_index += 1
+
+    # add the number that is zero to the new list at the end
+    for i in range(new_index, len(arr)):
+        arr[i] = 0
+
+    return arr
 
 
 if __name__ == '__main__':
