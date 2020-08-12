009c481819343145571bc45d9e017e29de761979
Vici Shaweddy <vshaweddy@gmail.com>
2020-08-11 23:15:29 -0400

    product of all other numbers

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -3,9 +3,29 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def product_of_all_other_numbers(arr):
-    # Your code here
+    # check the length of the array
+    n = len(arr)
 
-    pass
+    # create lists for left and right products and also for the result
+    left = [1 for _ in range(n)]
+    right = [1 for _ in range(n)]
+    result = []
+
+    # calculate the left product first
+    for i in range(1, n):
+        left[i] = left[i - 1] * arr[i - 1]
+
+    # calculate the right product
+    # starts from the right side and because it's python you need to -1 
+    # and another -1 because we skip the last number so it becomes -2
+    for i in range(n-2, -1, -1):
+        right[i] = right[i + 1] * arr[i + 1]
+
+    # combine left and right by multiply and store in a new list
+    for i in range(n):
+        result.append(left[i] * right[i])
+
+    return result
 
 
 if __name__ == '__main__':
